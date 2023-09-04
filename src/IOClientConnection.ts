215908f2d2d3183434d842e5b33c6b49434495e1
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2023-09-04 11:45:45 +0200

    revert socket listeners

--- src/IOClientConnection.ts
+++ src/IOClientConnection.ts
@@ -145,16 +145,5 @@ export class IOClientConnection extends BaseConnection {
 				(error: any) => result(error, null)
 			);
 		});
-
-		// Listen and re-transmit events from manager.
-		this.socket.io.on("error", (error: Error) => {
-			this.emit("error", (error))
-		})
-		this.socket.io.on("reconnect_attempt", (attempt: number) => {
-			this.emit("reconnect_attempt", (attempt))
-		})
-		this.socket.io.on("reconnect_error", (error: Error) => {
-			this.emit("reconnect_error", (error))
-		})
 	}
 }
